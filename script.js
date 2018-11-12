394c449f72e5e3d124dcc8f972c0a8d417c299b2
Glenn Dixon <glenndixon19@gmail.com>
2018-11-11 21:23:25 -0600

    game function takes in the user and computer choice and compare the two choices in a switch statement to determine the winner

--- script.js
+++ script.js
@@ -13,10 +13,26 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice());
 
 function game(userChoice) {
-    
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win();
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;
+    }
 }
 
 function main() {
